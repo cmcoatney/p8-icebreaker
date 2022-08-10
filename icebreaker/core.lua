4b18ec2519c2b63d23d465b78cea68ddb0f2e5d8
Matthew Coatney <mcoatney@bitwiseindustries.com>
2022-08-09 23:27:45 -0700

    feat(state): Implements basic state

--- icebreaker/core.lua
+++ icebreaker/core.lua
@@ -1,6 +1,16 @@
 -- CORE
-function _init()
 
+state={}
+function _init()
+  state.cell_size = 32
+  state.map       = {}
+  state.mobs      = {}
+  state.debug     = ""
+  state.error     = ""
+  state.UP        = 1
+  state.DOWN      = 2
+  state.RIGHT     = 3
+  state.LEFT      = 4
 end
 
 function _update60()
